6226812be90abd987c035aead681b8755c0ca7d6
markziemann <mark.ziemann@gmail.com>
2019-05-24 11:51:24 +1000

    added myscript to repo

---
+++ myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "hello world!"
+
+formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa 
+msbar -sequence mysample.fa -count 200 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
+blast2 -p blastn -e 0.001 -m8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
+
