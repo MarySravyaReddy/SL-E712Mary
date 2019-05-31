887f8b3743b853ea8e4304222cbbd3c0dbf5dd5b
markziemann <mark.ziemann@gmail.com>
2019-06-01 08:12:15 +1000

    added my script to repo

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,12 @@
+
 #!/bin/bash
 
 echo "hello world!"
- for i in {1..10}; do
+
+for i in {1..10}; do
+
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa 
-msbar -sequence 6.fa -count 200 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
+msbar -sequence mysample.fa -count 240 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
 blast2 -p blastn -e 0.001 -m8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
 
 done
